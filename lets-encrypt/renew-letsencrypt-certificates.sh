ecbc3bccec4e7836045b3bdc8190906ed909b7b5
Martin Riedel <mail@mrtnrdl.de>
2016-05-12 22:15:07 +0200

    Removed suffix after cleaning up letsencrypt installation...

--- lets-encrypt/renew-letsencrypt-certificates.sh
+++ lets-encrypt/renew-letsencrypt-certificates.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 domain="blog.mrtnrdl.de"
-suffix="-0001"
+#suffix="0002"
 
 PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin
 
 letsencrypt certonly
 
-uberspace-add-certificate -k ~/.config/letsencrypt/live/$domain$suffix/privkey.pem -c ~/.config/letsencrypt/live/$domain$suffix/cert.pem
+uberspace-add-certificate -k ~/.config/letsencrypt/live/$domain/privkey.pem -c ~/.config/letsencrypt/live/$domain/cert.pem
